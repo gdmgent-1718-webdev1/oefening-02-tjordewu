fb916c44522bb405d3b41710580b367a85242ef9
gdm-1718-tjordewu <tjordewu@student.arteveldehs.beecho>
2017-10-05 10:06:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $string1 = "Hello ";
+        $string2 = "World";
+        echo $string1 . $string2;
+
+    ?>
+    
+</body>
+</html>
